84ae6359c9f1a8d7f854efbcc25989f680f0d5c4
guidiamond <38350062+guidiamond@users.noreply.github.com>
2018-05-19 17:49:27 -0300

    Update background.py

--- background.py
+++ background.py
@@ -1,5 +1,6 @@
 #ADICIONAR O CODIGO A SEGUIR NO INCIO DE TODA PAGINA EM QUE QUEIRA ADICIONAR O BACKGROUND, DEPOIS DE tk.Frame.__init__(self, parent) 
-
+#PARA ESCOLHER O BACKGROUND DA LABEL EX:tk.Label(self, text='blablabla',bg = 'cor')
+#PARA ESCOLHER A COR DO TEXTO DA LABEL EX:tk.Label(self, text='blablabla', fg='cor')
 
 photo = tk.PhotoImage(file='cash.png')
         BGlabel = tk.Label(self, image=photo)
