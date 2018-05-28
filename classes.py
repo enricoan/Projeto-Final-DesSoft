e1d2c0c4c1702bb79fd358153f6ead5d0db426f4
Jadson Silva <38050693+jadsonsoj@users.noreply.github.com>
2018-05-28 09:28:40 -0300

    Update classes.py
    Alteração na formula para compra e venda

--- classes.py
+++ classes.py
@@ -13,13 +13,13 @@ class Clientes: #Aqui definimos uma classe chamada posicao
         #Ou seja, temos 400 ações da Tesla
         #Dinheiro é o saldo disponível no banco para compra de ações
     def compra (self, acao, preco, quantidade): #O metodo compra recebe de acao, quantidade e preco
-        self.saldo+=quantidade*preco #Uma quantidade do nosso dinheiro é removida
+        self.saldo-=quantidade*preco #Uma quantidade do nosso dinheiro é removida
         if acao in self.carteira: #testamos se a acao já existe no dicionario
             self.carteira[acao]+=quantidade
         else: #se ela n existir colocamos ela lá
             self.carteira[acao]=quantidade
     def venda(self, acao, preco, quantidade):
-        self.saldo-=quantidade*preco
+        self.saldo+=quantidade*preco
         if acao in self.carteira: #testamos se a acao já existe no dicionario
             self.carteira[acao]-=quantidade
         else: 
