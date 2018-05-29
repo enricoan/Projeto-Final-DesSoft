4957eebceaa1936e7e79ba2709be8917a5e8aa6c
Jadson Silva <38050693+jadsonsoj@users.noreply.github.com>
2018-05-29 04:26:36 -0300

    Update classes.py
    adicao de atributos

--- classes.py
+++ classes.py
@@ -8,10 +8,12 @@ class Clientes: #Aqui definimos uma classe chamada posicao
     def __init__(self):
         self.carteira={} 
         self.saldo=1E4
-        #Essa classe possui dois atributos: posição, um dicionário e dinheiro, um inteiro
-        #posicao é o dicionário estruturado em {acao:quantidade}, ex: {Tesla:400}
+        self.lucro=0
+        self.posicao=0 #momento do jogo associado 
+        
+        #carteira é o dicionário estruturado em {acao:quantidade}, ex: {Tesla:400}
         #Ou seja, temos 400 ações da Tesla
-        #Dinheiro é o saldo disponível no banco para compra de ações
+        
     def compra (self, acao, preco, quantidade): #O metodo compra recebe de acao, quantidade e preco
         self.saldo-=quantidade*preco #Uma quantidade do nosso dinheiro é removida
         if acao in self.carteira: #testamos se a acao já existe no dicionario
